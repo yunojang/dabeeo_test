4f2f39609d3032cbb8db8f4f22788cb59d891820
yuno <hanganda23@naver.com>
2021-10-28 05:55:26 +0900

    style: Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
-function Header(props) {
-  return <div></div>;
+function Header() {
+  return (
+    <Container>
+      <div className="content">
+        <HomeLink href="/">
+          <Logo
+            src="https://www.dabeeo.com/wp-content/uploads/2021/09/21_dabeeo_orange.png"
+            alt="logo"
+          />
+        </HomeLink>
+      </div>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.header`
+  position: sticky;
+  top: 0;
+  border-bottom: 2px solid #eee;
+
+  .content {
+    max-width: 1300px;
+    margin: auto;
+    height: 58px;
+  }
+`;
+
+const HomeLink = styled.a`
+  display: inline-block;
+  padding: 8px;
+  padding-top: 12px;
+`;
+
+const Logo = styled.img`
+  width: 96px;
+  height: auto;
+  cursor: pointer;
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import GlobalStyle from "./style/GlobalStyle";
+
 import App from "./App";
+import Header from "./Components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById("root")
 );
